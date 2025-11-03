1d76ffcf441e97b0866f87906a987372bfe417eb
Sidhant Kumar <ksidhant193@gmail.com>
2025-11-03 05:54:34 -0400

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,8 +2,9 @@ const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
+const router = express.Router();
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   try {
     const top = encodeURIComponent(req.query.top || "This is top Text!");
     const bottom = encodeURIComponent(req.query.bottom || "This is bottom Text!");
@@ -22,5 +23,7 @@ app.get("/", (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
+
+app.use("/.netlify/functions/api", router);
 
 module.exports.handler = serverless(app);
