aa737fc879727c19d310bca9cfd3cd9a575ff5cd
Sidhant Kumar <ksidhant193@gmail.com>
2025-11-03 05:04:28 -0400

    added netlify essentials

---
+++ netlify/functions/api.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+
+app.get("/api/meme", (req, res) => {
+  try {
+    const top = encodeURIComponent(req.query.top || "This is top Text!");
+    const bottom = encodeURIComponent(req.query.bottom || "This is bottom Text!");
+    const template = req.query.template || "doge";
+
+    let memeURL;
+    if (template === "random") {
+      memeURL = `https://api.memegen.link/images/random/${top}/${bottom}.png`;
+    } else {
+      memeURL = `https://api.memegen.link/images/${template}/${top}/${bottom}.png`;
+    }
+
+    res.json({ url: memeURL });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+module.exports.handler = serverless(app);
